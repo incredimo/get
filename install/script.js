8512913e0c22ca818b4ad3beedf353e1a843b4dd
incredimo <aghil@xo.rs>
2024-05-17 20:08:15 +0400

    asda

--- install/script.js
+++ install/script.js
@@ -1,12 +1,2 @@
 // script.js
 
-document.addEventListener("DOMContentLoaded", function () {
-    const fileList = document.getElementById('fileList');
-    const files = ['script1.sh', 'script2.ps1', 'script3.py']; // List your files here
-    files.forEach(file => {
-      const listItem = document.createElement('li');
-      listItem.textContent = file;
-      fileList.appendChild(listItem);
-    });
-  });
-  
