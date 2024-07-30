c5720d468028dc271ae410e80eb0005c254d10c4
incredimo <aghil@xo.rs>
2024-07-30 15:34:48 +0400

    bump version

--- bump.ps1
+++ bump.ps1
@@ -4,15 +4,13 @@ function show($color, $message) {
     Write-Host $message -ForegroundColor $color
 }
 
-$green = "Green"
-$yellow = "Yellow"
-$red = "Red"# constants for colors
+ 
 
 
 
 
 
-show yellow "commiting changes"
+show "yellow" "commiting changes"
 git add .
 git commit -m "bump version"
 git push
